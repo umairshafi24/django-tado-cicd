8d8161ebe325b5292344b8561b044b2f7f4743d5
umairshafi24 <umiiwadud187@gmail.com>
2024-07-04 14:30:20 +0500

    Update views.py

--- todos/views.py
+++ todos/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 
 class IndexView(generic.ListView):
     template_name = 'todos/index.html'
-    context_object_name = 'todo_list-Umair'
+    context_object_name = 'todo_list'
 
     def get_queryset(self):
         """Return all the latest todos."""
